e6153320e24fba95b7008944ded7cd2143dd58d4
Simon Kurz <simonlukas.98@gmail.com>
2022-01-18 15:24:32 +0100

    Added basis for icons

---
+++ components/icons/Icon.tsx
@@ -0,0 +1,31 @@
+import {ComponentPropsWithoutRef} from "react";
+
+/**
+ * Available sizes.
+ */
+export type Sizes = 'small' | 'medium' | 'large';
+
+/**
+ * Props for Icons.
+ */
+export interface IconPropsNoDefault extends ComponentPropsWithoutRef<'svg'> {
+  size?: Sizes;
+}
+
+/**
+ * Available sizes for icons.
+ */
+export const sizes = {
+  small: 16,
+  medium: 24,
+  large: 32,
+}
+
+/**
+ * Default props for icons.
+ */
+export const defaultIconProps = {
+  size: 'medium',
+}
+
+export type IconProps = IconPropsNoDefault & typeof defaultIconProps;
