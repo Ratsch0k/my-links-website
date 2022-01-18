c7ea4b73d995bb862a65848c0f8cfd19cc85ccec
Simon Kurz <simonlukas.98@gmail.com>
2022-01-18 15:39:13 +0100

    Added links, imprint and licenses to main component

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+export interface ComponentProps {
+  id?: string;
+  className?: string;
+  style?: CSSProperties;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,79 @@
 import DotBackground from '../components/dot-bg';
+import Button from "../components/Button";
+import Logo from "../components/icons/Logo";
+import {useState} from "react";
+import Imprint from "./imprint";
+import Licenses from "./licenses";
 
+interface LinkItem {
+  name: string;
+  url: string;
+  icon?: JSX.Element;
+}
+
+const links: LinkItem[] = [
+  {
+    name: 'Portfolio (WIP)',
+    url: 'https://ratsch0k.github.io'
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/Ratsch0k',
+    icon: <img width={24} height={24} src='/GitHub-64px.png' />
+  }
+]
 
 export default function Home() {
+  const [openImprint, setOpenImprint] = useState<boolean>(false);
+  const [openLic, setOpenLic] = useState<boolean>(false);
+
   return (
     <>
       <div className='absolute center w-full h-full'>
-        <div id='text-paper' className='container bg-primary center elevation-8'>
-          <div className='white text-center'>
-            <h2>
-              Hier entsteht gerade mein Portfolio
-            </h2>
-            <h5>
-              Den aktuellen Stand dazu können sie unter <br></br><a target='blank' href='https://ratsch0k.github.io'>https://ratsch0k.github.io</a> einsehen.
-            </h5>
+        <div id='text-paper' className='bg-primary center elevation-8 p-16 container'>
+          <div className='text-primary text-center'>
+            <Logo style={{height: 64}} />
+            <h3 className='mb-32 mt-0 bold'>
+              Simon Kurz
+            </h3>
+            <h4>
+              Hi,<br/>ich studiere IT-Sicherheit an der TU Darmstadt im Master und interessiere mich hauptsächlich für Software Entwicklung und Cybersecurity.
+              <br />
+              Von Webseiten, über Apps und SysAdmin bis hin zu einer kleinen Game Engine habe ich in vielen Themen Erfahrung.
+              <br /><br />
+              Unten könnt ihr alle meine Links sehen.
+            </h4>
+
+            {
+              links.map(({name, url, icon}) =>
+                <Button key={`link-to-${name}`} className='w-full mt-16' onClick={() => window.open(url, '_blank')}>
+                  <div className='flex justify-center items-center'>
+                    <span className='icon mr-4' style={{color: 'inherit'}}>
+                      {icon}
+                    </span>
+                    {name}
+                  </div>
+                </Button>
+              )
+            }
           </div>
         </div>
       </div>
+      <div className='absolute bottom-0 right-0 flex bg-white p-4' style={{flexDirection: 'row'}}>
+        <div className='primary text-button' style={{marginRight: 8}} onClick={() => setOpenImprint(true)}>
+          Impressum
+        </div>
+        <div className='primary text-button' onClick={() => setOpenLic(true)}>
+          Lizensen
+        </div>
+      </div>
+      {
+        openImprint && <Imprint close={() => setOpenImprint(false)}/>
+      }
+      {
+        openLic && <Licenses close={() => setOpenLic(false)} />
+      }
+
       <DotBackground />
     </>
   );
